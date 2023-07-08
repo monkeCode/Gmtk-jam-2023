8ad37c6855d2db5d048c025444f24f583b4cbc40
AlexKhochenkov <alekshoch@yandex.ru>
2023-07-08 17:45:34 +0300

    BLYAT_NAKONEC_TO_ZACOMMITILOS

--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -2,6 +2,10 @@ extends Resource
 
 class_name Item
 
+@export var name:String
+@export_multiline var description:String
+@export var texture:AtlasTexture
+
 @export var cooldown : float
 
 @export var manacost : int
