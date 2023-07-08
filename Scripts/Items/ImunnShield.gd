3115e5c8c7de065e8259628bb0b19e3bb4cd8f4c
Georgy <georgy.tertychniy@yandex.ru>
2023-07-08 22:03:15 +0300

    imunne shield

--- Scripts/Items/ImunnShield.gd
+++ Scripts/Items/ImunnShield.gd
@@ -3,8 +3,13 @@ class_name ImmuneShield
 
 @export var imunneTime:float = 1
 
+var effect = preload("res://scenes/effects/shield.tscn")
+
 func use_item(caster):
 	var last_resist = caster.damage_resist
 	caster.damage_resist = 10000
+	var s = effect.instantiate()
+	caster.add_child(s)
 	await caster.get_tree().create_timer(imunneTime).timeout
 	caster.damage_resist = last_resist
+	caster.remove_child(s)
